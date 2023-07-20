3a0a703859bc1e73ffb4ef1f100ee8b0e01b665a
sentcf <139645418+sentcf@users.noreply.github.com>
2023-07-21 02:44:30 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,21 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR FIREBASE API KEY",
-  authDomain: "YOUR FIREBASE AUTHDOMAIN",
-  projectId: "YOUR FIREBASE PROJECTID",
-  storageBucket: "YOUR FIREBASE STORAGE BUCKET",
-  messagingSenderId: "YOUR FIREBASE MESSAGESENDER ID",
-  appId: "YOUR FIREBASE APPID"
+  apiKey: "AIzaSyCwL4mYYGeuqjkYgktTr3fEp4MlUc7GTm4",
+  authDomain: "hamza-portfolio-f30ba.firebaseapp.com",
+  projectId: "hamza-portfolio-f30ba",
+  storageBucket: "hamza-portfolio-f30ba.appspot.com",
+  messagingSenderId: "526692752996",
+  appId: "1:526692752996:web:f17741db0f840ad5ee4971",
+  measurementId: "G-TWBJEEF714"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+export {auth, provider}
